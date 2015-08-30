0cfe46ed016e754feabd6e8fbd96d5f787541de4
Espanta <abolfazli.s@gmail.com>
2015-08-30 14:01:47 -0700

    Load Commit

---
+++ Ch01/01_03/Ex01_03.R
@@ -0,0 +1,14 @@
+# R Statistics Essential Training
+# Ex01_03
+# Taking a first look at the interface
+
+# It allows to read data from keyboad in console and store them in X.
+# Finish data entry by pressing twice "Enter"
+x <- scan()
+
+# Entering an auto generated list of numbers from 1 to 10
+x <- 1:10
+
+# If I wanna give shape to my sequence, I can do it using below:
+x <- seq(13, 3, by = -3)
+
